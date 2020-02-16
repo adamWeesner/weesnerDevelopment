df3adee04fde8a4e40a368b69d76c3439f32724c
Adam Weesner <alweesner@gmail.com>
2020-02-16 04:51:19 -0600

    added docs to generics

--- backend/src/main/kotlin/Utils.kt
+++ backend/src/main/kotlin/Utils.kt
@@ -15,5 +15,40 @@ enum class Paths {
     socialSecurity, medicare, federalIncomeTax, taxWithholding
 }
 
-inline fun <reified T> String.fromJson() =
-    Moshi.Builder().add(KotlinJsonAdapterFactory()).build().adapter(T::class.java).fromJson(this)
+/**
+ * A moshi instance that can take a [ParameterizedType] if needed.
+ */
+inline fun <reified T> moshi(type: ParameterizedType? = null) =
+    Moshi.Builder().add(KotlinJsonAdapterFactory()).build().adapter<T>(type ?: T::class.java)
+
+/**
+ * Helper function that creates an object of type [T] from a json string.
+ */
+inline fun <reified T> String.fromJson(type: ParameterizedType? = null) = moshi<T>(type).fromJson(this)
+
+/**
+ * Helper function to convert an object to a json string.
+ */
+inline fun <reified T> T?.toJson() = moshi<T>().toJson(this)
+
+data class ServerError(
+    val status: String,
+    val statusCode: Int,
+    val message: String
+)
+
+/**
+ * Helper function to [respond] with a [ServerError].
+ */
+internal suspend fun ApplicationCall.respondServerError(error: Throwable) {
+    val httpStatus = HttpStatusCode.InternalServerError
+    respond(httpStatus, ServerError(httpStatus.description, httpStatus.value, error.localizedMessage))
+}
+
+/**
+ * Helper function to [respond] with an [InvalidUserException] with the given [reason].
+ */
+internal suspend fun ApplicationCall.respondAuthorizationIssue(reason: InvalidUserReason) {
+    val httpStatus = HttpStatusCode.Unauthorized
+    respond(httpStatus, InvalidUserException(request.uri, httpStatus.value, reason.code).toJson())
+}
--- businessRules/src/main/kotlin/generics/Generics.kt
+++ businessRules/src/main/kotlin/generics/Generics.kt
@@ -3,15 +3,23 @@ package generics
 import com.squareup.moshi.JsonClass
 import org.jetbrains.exposed.sql.Table
 
+/**
+ * Generic [Table] that has [id], [dateCreated], and [dateUpdated] added by default.
+ */
 open class IdTable : Table() {
     val id = integer("id").primaryKey().autoIncrement()
     val dateCreated = long("dateCreated")
     val dateUpdated = long("dateUpdated")
 }
 
+/**
+ * Generic item to map to the [IdTable].
+ */
 @JsonClass(generateAdapter = true)
 open class GenericItem(
     open val id: Int?,
     open val dateCreated: Long,
     open val dateUpdated: Long
 )
+
+class InvalidAttributeException(value: String) : IllegalArgumentException("$value is required but missing or invalid")

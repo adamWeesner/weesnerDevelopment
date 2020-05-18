6cd811bcd06749a417a0b232d9c8eb1f4d094841
Adam Weesner <alweesner@gmail.com>
2020-05-17 22:58:06 -0500

    added history for colors, can now insert colors..

--- billMan/src/main/kotlin/colors/ColorsService.kt
+++ billMan/src/main/kotlin/colors/ColorsService.kt
@@ -4,8 +4,8 @@ import HistoryTypes
 import dbQuery
 import generics.GenericService
 import history.HistoryService
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.select
+import model.ChangeType
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import shared.billMan.Color
 
@@ -15,7 +15,38 @@ class ColorsService(
     ColorsTable
 ) {
     suspend fun getByBill(id: Int) =
-        dbQuery { table.select { (table.billId eq id) }.mapNotNull { to(it) } }.first()
+        dbQuery { table.select { (table.billId eq id) }.mapNotNull { to(it) } }.firstOrNull()
+            ?: throw IllegalArgumentException("No color found for bill..")
+
+    suspend fun deleteForBill(billId: Int) = dbQuery {
+        table.select { (table.billId eq billId) }.mapNotNull { to(it).id }
+    }.forEach { delete(it) { table.id eq it } }
+
+    @Deprecated("Use add(billId, color) instead", ReplaceWith("add(bill.id, color)"), DeprecationLevel.ERROR)
+    override suspend fun add(item: Color): Color? = null
+
+    suspend fun add(bill: Int, item: Color): Color? {
+        var key = 0
+
+        dbQuery {
+            key = table.insert {
+                it[billId] = bill
+                it.assignValues(item)
+                it[dateCreated] = System.currentTimeMillis()
+                it[dateUpdated] = System.currentTimeMillis()
+            } get table.id
+        }
+        return getSingle { table.id eq key }?.also {
+            onChange(ChangeType.Create, key, it)
+        }
+    }
+
+    override suspend fun delete(id: Int, op: SqlExpressionBuilder.() -> Op<Boolean>): Boolean {
+        historyService.run {
+            getFor(HistoryTypes.Bill.name, id).mapNotNull { it.id }.forEach { delete(it) { table.id eq it } }
+        }
+        return super.delete(id, op)
+    }
 
     override suspend fun to(row: ResultRow) = Color(
         id = row[ColorsTable.id],
--- billMan/src/main/kotlin/colors/ColorsTable.kt
+++ billMan/src/main/kotlin/colors/ColorsTable.kt
@@ -1,12 +1,15 @@
 package colors
 
 import bills.BillsTable
+import generics.HistoricTable
 import generics.IdTable
+import history.HistoryTable
 
-object ColorsTable : IdTable() {
+object ColorsTable : IdTable(), HistoricTable {
     val billId = integer("billId") references BillsTable.id
     val red = integer("red")
     val green = integer("green")
     val blue = integer("blue")
     val alpha = integer("alpha")
+    override val history = (integer("historyId") references HistoryTable.id).nullable()
 }

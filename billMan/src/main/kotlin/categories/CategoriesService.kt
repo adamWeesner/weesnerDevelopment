f4813d62fc8480cb9f0741defa9c35ae18d9dbae
Adam Weesner <alweesner@gmail.com>
2020-05-17 23:02:32 -0500

    deleting a category removes its history

--- billMan/src/main/kotlin/categories/CategoriesService.kt
+++ billMan/src/main/kotlin/categories/CategoriesService.kt
@@ -4,7 +4,9 @@ import HistoryTypes
 import auth.UsersService
 import generics.GenericService
 import history.HistoryService
+import org.jetbrains.exposed.sql.Op
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SqlExpressionBuilder
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import shared.billMan.Category
 
@@ -14,6 +16,13 @@ class CategoriesService(
 ) : GenericService<Category, CategoriesTable>(
     CategoriesTable
 ) {
+    override suspend fun delete(id: Int, op: SqlExpressionBuilder.() -> Op<Boolean>): Boolean {
+        historyService.run {
+            getFor(HistoryTypes.Bill.name, id).mapNotNull { it.id }.forEach { delete(it) { table.id eq it } }
+        }
+        return super.delete(id, op)
+    }
+
     override suspend fun to(row: ResultRow) = Category(
         id = row[CategoriesTable.id],
         owner = row[CategoriesTable.ownerId]?.let { usersService.getUserByUuidRedacted(it) },

1cd99bc67a2e95198e40bb368a2d5dd0b3d041a7
Adam Weesner <alweesner@gmail.com>
2020-08-11 22:46:33 -0500

    added base router and service, updated shared lib

---
+++ businessRules/src/main/kotlin/BaseRouter.kt
@@ -0,0 +1,115 @@
+import io.ktor.application.call
+import io.ktor.request.receive
+import io.ktor.routing.*
+import shared.base.GenericItem
+import shared.base.GenericResponse
+import shared.base.Response
+import shared.base.Response.Companion.BadRequest
+import shared.base.Response.Companion.NoContent
+import shared.base.Response.Companion.NotFound
+import shared.base.Response.Companion.Ok
+import kotlin.reflect.KType
+
+abstract class BaseRouter<I : GenericItem, S : Service<I>>(
+    private val response: GenericResponse<I>,
+    override val service: S,
+    override val kType: KType
+) : Router<I, S> {
+    override fun Route.setupRoutes() {
+        route("/$basePath") {
+            addRequest()
+            getRequest()
+            updateRequest()
+            deleteRequest()
+        }
+    }
+
+    override fun Route.addRequest() {
+        post {
+            val body = call.receive<I>(kType)
+
+            val response =
+                when (val addedItem = service.add(body)?.let {
+                    if (it != -1)
+                        addExtras(it, body)
+                    else
+                        it
+                }) {
+                    null -> BadRequest("Failed to add $body.")
+                    -1 -> Response.Conflict("Item with name $body already in database.")
+                    else -> Response.Created("Added item to database with id $addedItem.")
+                }
+
+            call.respond(response)
+        }
+    }
+
+    override fun Route.getRequest() {
+        get {
+            val itemId = call.parameters["id"]
+
+            val items =
+                (if (itemId == null) service.getAll()
+                else listOf(service.get {
+                    service.table.id eq itemId.toInt()
+                }))?.filterNotNull()
+
+            val response =
+                if (items.isNullOrEmpty()) NoContent(response)
+                else Ok(response.let {
+                    it.items = items
+                    it
+                })
+
+            call.respond(response)
+        }
+    }
+
+    override fun Route.updateRequest() {
+        put {
+            val body = call.receive<I>(kType)
+
+            val response =
+                if (body.id == null) {
+                    BadRequest("Cannot update item without id")
+                } else {
+                    when (val updatedItem = service.update(body) {
+                        service.table.id eq body.id!!
+                    }?.let {
+                        if (it != -1)
+                            updateExtras(it, body)
+                        else
+                            it
+                    }) {
+                        null -> BadRequest("Failed to update $body.")
+                        else -> Ok("Updated item to database with id $updatedItem")
+                    }
+                }
+
+            call.respond(response)
+        }
+    }
+
+    override fun Route.deleteRequest() {
+        delete {
+            val itemId = call.parameters["id"]
+                ?: return@delete call.respond(BadRequest("?id=(itemId) is needed."))
+
+            val item = service.get { service.table.id eq itemId.toInt() }
+                ?: return@delete call.respond(NotFound("Could not delete item with id $itemId"))
+
+            val delete = service.delete(item) {
+                service.table.id eq itemId.toInt()
+            }
+
+            val response =
+                if (delete) Ok("Successfully removed item with id $itemId.")
+                else NotFound("Could not delete item with id $itemId")
+
+            call.respond(response)
+        }
+    }
+
+    open suspend fun addExtras(id: Int, item: I, block: () -> Int? = { 0 }): Int? = 0
+    open suspend fun updateExtras(id: Int, item: I, block: () -> Int? = { 0 }): Int? = 0
+}
